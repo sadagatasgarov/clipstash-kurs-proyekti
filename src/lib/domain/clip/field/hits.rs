7a7b389e842a603abe3ccc9677528e9a403a1762
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-12-25 01:50:38 +0400

    content ve hhitsi yazdiq

---
+++ src/lib/domain/clip/field/hits.rs
@@ -0,0 +1,14 @@
+use super::super::ClipError;
+use serde::{Deserialize, Serialize};
+use derive_more::Constructor;
+
+
+#[derive(Clone, Constructor, Debug, Deserialize, Serialize)]
+pub struct Hits(u64);
+
+
+impl Hits {
+    pub fn into_inner(self) -> u64 {
+        self.0
+    }
+}
