9ff4e3f4ebcb16e6e430d26d816d0ba3c6fb2053
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-12-25 01:32:56 +0400

    Errorlari yaziriq

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -1,6 +1,26 @@
-use serde::{Deserialize, Serialize};
-
 pub mod field;
+
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+
+#[derive(Debug, Error)]
+pub enum ClipError {
+    #[error("invalid password: {0}")]
+    InvalidPassword(String),
+    #[error("invalid title: {0}")]
+    InvalidTitle(String),
+    #[error("Bos Content")]
+    EmptyContent,
+    #[error("invalid Date: {0}")]
+    InvalidDate(String),
+    #[error("Date parse error: {0}")]
+    Dateparse(#[from] chrono::ParseError),
+    #[error("Id parse error: {0}")]
+    Id(#[from] uuid::Error),
+    #[error("Hits parse error : {0}")]
+    Hits(#[from] std::num::TryFromIntError)
+}
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Clip {
