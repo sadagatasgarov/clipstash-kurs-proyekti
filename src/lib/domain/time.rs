7c05252f1f444001f81bc129fa82f08894e7d79a
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-12-25 12:31:47 +0400

    domaine timers add olundu

---
+++ src/lib/domain/time.rs
@@ -0,0 +1,34 @@
+use chrono::{DateTime, NaiveDateTime, Utc};
+use derive_more::From;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+#[derive(Debug, Clone, From, Deserialize, Serialize)]
+pub struct Time(DateTime<Utc>);
+
+impl Time {
+    pub fn into_inner(self) -> DateTime<Utc> {
+        self.0
+    }
+
+    pub fn timestapm(&self) -> i64 {
+        self.0.timestamp()
+    }
+
+    pub fn from_naive_utc(datetime: NaiveDateTime) -> Self {
+        Time(DateTime::from_naive_utc_and_offset(datetime, Utc))
+    }
+}
+
+impl FromStr for Time {
+    type Err = chrono::ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        //2022-05-22 s ile gelecek
+        match format!("{}T00::00:0Z", s).parse::<DateTime<Utc>>() {
+            Ok(time) => Ok(time.into()),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+
