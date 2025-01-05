16184dbae4676dd0139fddfcc4167506767e6576
sadagatasgarov <sadagat.asgarov@gmail.com>
2025-01-05 20:53:28 +0400

    API-client

--- src/lib/domain/clip/field/password.rs
+++ src/lib/domain/clip/field/password.rs
@@ -1,36 +1,43 @@
-use super::super::ClipError;
-use rocket::form::{self, FromFormField, ValueField};
+use crate::domain::clip::ClipError;
 use serde::{Deserialize, Serialize};
+use rocket::form::{self, FromFormField, ValueField};
 use std::str::FromStr;
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, PartialOrd)]
+/// The password field for a [`Clip`](crate::domain::clip::Clip).
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, PartialOrd)]
 pub struct Password(Option<String>);
 
 impl Password {
+    /// Create a new `Password` field.
+    ///
+    /// There are no complexity checks currently implemented, however, if
+    /// some were to be added, then a [`ClipError`] should be returned.
     pub fn new<T: Into<Option<String>>>(password: T) -> Result<Self, ClipError> {
         let password: Option<String> = password.into();
         match password {
-            Some(p) => {
-                if !p.trim().is_empty() {
-                    Ok(Self(Some(p)))
+            Some(password) => {
+                if !password.trim().is_empty() {
+                    Ok(Self(Some(password)))
                 } else {
                     Ok(Self(None))
                 }
             }
-            // None => Err(ClipError::InvalidPassword("Password xetasi".to_string()))
-            None => Ok(Self(None)),
+            None => Ok(Self(None))
         }
     }
 
+    /// Return the underlying [`String`].
     pub fn into_inner(self) -> Option<String> {
         self.0
     }
 
+    /// Returns whether a password has been set.
     pub fn has_password(&self) -> bool {
         self.0.is_some()
     }
 }
 
+/// The Default implementation is no password.
 impl Default for Password {
     fn default() -> Self {
         Self(None)
@@ -48,6 +55,7 @@ impl FromStr for Password {
 impl<'r> FromFormField<'r> for Password {
     fn from_value(field: ValueField<'r>) -> form::Result<'r, Self> {
         Ok(Self::new(field.value.to_owned())
-            .map_err(|e| form::Error::validation(format!("{}", e)))?)
+            .map_err(|e| form::Error::validation(format!("{}", e)))?
+        )
     }
 }
